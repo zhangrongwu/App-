c09ec855005b48644a2e7744752c3864ab53b755
倒茄子 <15600257760@163.com>
2014-10-31 13:44:51 +0800

    Initial Commit

---
+++ LightApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LightApp
+//
+//  Created by malong on 14/10/31.
+//  Copyright (c) 2014年 malong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LightApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LightApp
+//
+//  Created by malong on 14/10/31.
+//  Copyright (c) 2014年 malong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
